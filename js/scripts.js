6178acc1a8ae598e9243d3821377491e42da9bdb
glen <glen_sale@yahoo.com>
2018-10-26 15:07:19 -0700

    it will add append affect

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,15 @@ var output = []
 
 function Enternumbers(input) {
   for (var index = 0; index <= input; index++) {
-      
+
     if (index % 3 === 0 && index !== 0) {
       output= "I'm sorry, Dave. I'm afraid I can't do that.";
-  } else if (index.toString().includes("0")) {
-    output= "Beep!";
-  } else if (index.toString().includes("1")) {
+  } else if(index.toString().includes("1")) {
     output= "Boop!";
+  } else if (index.toString().includes("0")) {
+  output= "Beep!";
   } else {
+    
 
   }
 }
@@ -20,6 +21,6 @@ function Enternumbers(input) {
       event.preventDefault();
       var input = parseInt($("#inputnumbers").val());
       Enternumbers(input);
-        $("#output").text(output);
+        $("#output").append(output);
       });
     });
