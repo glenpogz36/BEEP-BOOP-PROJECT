dce71e4eb0d191ac8e612bb7e1edfc4d10a38bad
glen <glen_sale@yahoo.com>
2018-10-26 10:46:05 -0700

    it will try if else statement to return an output but somehow it's returning continues numbers(bug)

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,29 @@
-var encrypt = function(numbers){
-  var digits = [0,1,3];
+var output = [];
+var numberone = /[1]/g;
+var numberzero = /[0]/g;
 
-};
+function Enternumbers(input) {
+  for (var index = 0; index <= input; index++) {
+    if (index.toString().match(numberone)) {
+      return output.push("Boop!")
+    } else if (index.toString().match(numberzero)) {
+      return output.push("Beep!")
+    } else if (index % 3 === 0) {
+      return output.push("I'm sorry, Dave. I'm afraid I can't do that.")
+    } else {
+      return output.push(index)
+      }
+    }
+  }
 
 
-
-
-$(document).ready(function(){
-  $("#form").submit(function(event){
-    var numbers = ($("#inputnumbers").val());
-
-
-
-    $("#output").text();
+$(document).ready(function() {
+  $("#form").submit(function(event) {
     event.preventDefault();
-
+    var input = parseInt($("#inputnumbers").val());
+    var result = Enternumbers(input);
+    output.forEach(function(output) {
+      $("#output").text(result);
+    });
   });
 });
