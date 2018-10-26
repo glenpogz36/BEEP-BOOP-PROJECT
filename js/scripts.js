05d2781e513abda80d168ad5821ccdfb5f4bd1b4
glen <glen_sale@yahoo.com>
2018-10-26 10:58:52 -0700

    it will fix the bug by changing the value of text to the output, however the argument and the result is not 100% true yet for number 1

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,13 @@ var numberzero = /[0]/g;
 function Enternumbers(input) {
   for (var index = 0; index <= input; index++) {
     if (index.toString().match(numberone)) {
-      return output.push("Boop!")
+    output.push("Boop!")
     } else if (index.toString().match(numberzero)) {
-      return output.push("Beep!")
+     output.push("Beep!")
     } else if (index % 3 === 0) {
-      return output.push("I'm sorry, Dave. I'm afraid I can't do that.")
+     output.push("I'm sorry, Dave. I'm afraid I can't do that.")
     } else {
-      return output.push(index)
+     output.push(index)
       }
     }
   }
@@ -21,9 +21,7 @@ $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
     var input = parseInt($("#inputnumbers").val());
-    var result = Enternumbers(input);
-    output.forEach(function(output) {
-      $("#output").text(result);
+    Enternumbers(input);
+      $("#output").text(output);
     });
   });
-});
