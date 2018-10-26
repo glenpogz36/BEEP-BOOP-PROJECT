df831320d1b5551800be64a316cc5c329c3e3809
glen <glen_sale@yahoo.com>
2018-10-26 12:59:21 -0700

    it will try a diffrent method (fail)

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,25 @@
-var output = [];
-var numberone = /[1]/g;
-var numberzero = /[0]/g;
+var output = []
 
 function Enternumbers(input) {
-  for (var index = 0; index <= input; index++) {
-    if (index.toString().match(numberone)) {
-    output.push("Boop!")
-  } else if (index.toString().match(numberzero)) {
-     output.push("Beep!")
-    } else if (index % 3 === 0) {
-     output.push("I'm sorry, Dave. I'm afraid I can't do that.")
-    } else {
-     output.push(index)
-      }
-    }
+  for (var index=0; index<=input; index++) {
+    output.push(index);
+    if (output[index] % 3 === 0) {
+      output[index] = "I'm sorry, Dave. I'm afraid I can't do that.";
+  } else if (index.toString().includes("0")) {
+    output[index] = "Beep!";
+  } else if (index.toString().includes("1")) {
+    output[index] = "Boop!";
+  } else {
+
   }
+}
+}
 
-
-$(document).ready(function() {
-  $("#form").submit(function(event) {
-    event.preventDefault();
-    var input = parseInt($("#inputnumbers").val());
-    Enternumbers(input);
-      $("#output").text(output);
+  $(document).ready(function() {
+    $("#form").submit(function(event) {
+      event.preventDefault();
+      var input = parseInt($("#inputnumbers").val());
+      Enternumbers(input);
+        $("#output").text(output);
+      });
     });
-  });
