62094b94b1b3435534e07f7532eee0c85e4f8111
glen <glen_sale@yahoo.com>
2018-10-26 10:03:24 -0700

    it will attempt to add an array structure for looping later on

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+var encrypt = function(numbers){
+  var digits = [0,1,3];
+
+};
+
+
+
+
+$(document).ready(function(){
+  $("#form").submit(function(event){
+    var numbers = ($("#inputnumbers").val());
+
+
+
+    $("#output").text();
+    event.preventDefault();
+
+  });
+});
