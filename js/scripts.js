4377965b063d8dfb509f60b26188cd9e2f3927ee
glen <glen_sale@yahoo.com>
2018-11-01 22:12:13 -0700

    it will fix scripts, spacing, and looping array and giving back the correct values for each

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,23 @@
-var output = []
 
 function Enternumbers(input) {
   for (var index = 0; index <= input; index++) {
 
     if (index % 3 === 0 && index !== 0) {
-      output= "I'm sorry, Dave. I'm afraid I can't do that." + " ";
-  } else if(index.toString().includes("1")) {
-    output= "Boop!" + " ";
-  } else if (index.toString().includes("0")) {
-  output= "Beep!" + " ";
-  } else {
+      $("#output").append(" " + "I'm sorry, Dave. I'm afraid I can't do that");
+    }else if (index.toString().includes("1")){
+      $("#output").append(" " + "Boop!");
+    }else if (index.toString().includes("0")){
+      $("#output").append(" " + "Beep!");
+    }else {
+      $("#output").append(" " + index)
+    }
+  };
+};
 
-
-  }
-}
-}
-
-  $(document).ready(function() {
-    $("#form").submit(function(event) {
-      event.preventDefault();
-      var input = parseInt($("#inputnumbers").val());
-      Enternumbers(input);
-        $("#output").append(output);
-      });
-    });
+$(document).ready(function() {
+  $("#form").submit(function(event) {
+    event.preventDefault();
+    var input = parseInt($("#inputnumbers").val());
+    Enternumbers(input);
+  });
+});
